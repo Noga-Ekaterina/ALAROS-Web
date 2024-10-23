ea4add30fe6a1bed59eb07e9235dbe51ca4e98f8
Noga-Ekaterina <katrin210607@gmail.com>
2024-10-23 23:42:23 +0300

    доделала первый экран фестиваля

--- src/components/_festival/festival-main-screen/FestivalMainScreen.tsx
+++ src/components/_festival/festival-main-screen/FestivalMainScreen.tsx
@@ -6,10 +6,22 @@ const FestivalMainScreen = () => {
         <div className="festival-main-screen__block-text">
           <h1 className="festival-main-screen__title">Национальная премия России по ландшафтной архитектуре <br/>
             и садово-парковому искусству — главное событие года в отрасли</h1>
+
+          <a href="" className="festival-main-screen__link link-underline festival-main-screen__link--1">Расписание защит 2024</a>
         </div>
 
         <div className="festival-main-screen__img-wrapp">
           <img src="/Assets/Pages/Festival/Images/Main.png" alt="" className="main-screen__img festival-main-screen__img"/>
+          <div className="festival-main-screen__btns">
+            <div className="festival-main-screen__btn festival-main-screen__btn--red">
+              <h2 className="festival-main-screen__title2">Деловая <br/>
+                программа</h2>
+              <a href="" className="festival-main-screen__link link-underline">Изучить</a></div>
+            <div className="festival-main-screen__btn festival-main-screen__btn--green">
+              <h2 className="festival-main-screen__title2">Архив <br/>
+                премии</h2>
+              <a href="" className="festival-main-screen__link link-underline">Смотреть работы</a></div>
+          </div>
         </div>
       </div>
   );
