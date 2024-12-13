5c658476c4e677eb14761aad177371f0a43db6b5
Noga-Ekaterina <katrin210607@gmail.com>
2024-12-14 02:33:42 +0300

    вынесла отдельно логику слайдера

--- src/components/_home/home-news-slider/HomeNewsSlider.tsx
+++ src/components/_home/home-news-slider/HomeNewsSlider.tsx
@@ -1,78 +1,38 @@
-import React, {useRef, useState} from 'react';
+import React from 'react';
 import "./home-news-slider.scss"
-import {Swiper, SwiperSlide} from "swiper/react";
+import {SwiperSlide} from "swiper/react";
 import pagesData from "../../../store/pagesData";
 import {observer} from "mobx-react-lite";
 import NewsItem from "../../_news/news-item/NewsItem";
-import {Mousewheel} from "swiper/modules";
-import {ReactSVG} from "react-svg";
-import {SwiperRef} from "swiper/swiper-react";
-import {SwiperNavigation} from "../../../utils/SwiperNavigation";
 import classNames from "classnames";
-import {useMediaQuery} from "react-responsive";
+import BigSlider from "../../big-slider/BigSlider";
 
 const HomeNewsSlider = () => {
-  const mobileScreen = useMediaQuery({maxWidth: 660});
   const {newsPages}= pagesData
-  const swiperRef=useRef<SwiperRef | null>(null);
-  const swiperNav= new SwiperNavigation(swiperRef)
-  const [swiperIsStart, setSwiperIsStart] = useState(true)
-  const [swiperIsEnd, setSwiperIsEnd] = useState(false)
   const arrIndexSmall= [2, 5, 7, 10]
 
-  function togleSwiper  (){
-    setSwiperIsStart(swiperNav.isStart())
-    setSwiperIsEnd(swiperNav.isEnd(mobileScreen? 1:2))
-  }
+  if (!newsPages[1]) return <div/>
 
   return (
       <div className="home-news-slider">
         <div className="container">
           <h2 className="home-news-slider__title">Новости</h2>
-          <div className="home-news-slider__wrapp">
-            <button
-                className={classNames(
-                    "home-news-slider__btn",
-                    "home-news-slider__btn--prev",
-                    swiperIsStart && "btn--disable"
-                )}
-                onClick={() => swiperNav.goToPrev()}
-            >
-              <ReactSVG src="/Assets/Icons/arrow.svg"/>
-            </button>
-            <Swiper
-                slidesPerView="auto"
-                mousewheel={{sensitivity: 5000}}
-                modules={[Mousewheel]}
-                spaceBetween={"10rem"}
-                ref={swiperRef}
-                onActiveIndexChange={togleSwiper}
-            >
-              {
-                  newsPages["1"] && newsPages["1"].map((news, index) => (
-                      <SwiperSlide
-                          key={`news-slide-${news.slug}`}
-                          className={classNames(
-                              "home-news-slider__slide",
-                              arrIndexSmall.includes(index+1) && "home-news-slider__slide--small"
-                          )}
-                      >
-                        <NewsItem news={news}/>
-                      </SwiperSlide>
-                  ))
-              }
-            </Swiper>
-            <button
-                className={classNames(
-                    "home-news-slider__btn",
-                    "home-news-slider__btn--next",
-                    swiperIsEnd && "btn--disable"
-                )}
-                onClick={() => swiperNav.goToNext()}
-            >
-              <ReactSVG src="/Assets/Icons/arrow.svg"/>
-            </button>
-          </div>
+
+          <BigSlider>
+            {
+                newsPages["1"].map((news, index) => (
+                    <SwiperSlide
+                        key={`news-slide-${news.slug}`}
+                        className={classNames(
+                            "home-news-slider__slide",
+                            arrIndexSmall.includes(index + 1) && "home-news-slider__slide--small"
+                        )}
+                    >
+                      <NewsItem news={news}/>
+                    </SwiperSlide>
+                ))
+            }
+          </BigSlider>
         </div>
       </div>
   );
---
+++ src/components/big-slider/BigSlider.tsx
@@ -0,0 +1,64 @@
+import React, {useRef, useState} from 'react';
+import "./big-slider.scss"
+import {IWithChildren, IWithClass} from "../../types/tehnic";
+import classNames from "classnames";
+import {ReactSVG} from "react-svg";
+import {Swiper, SwiperRef} from "swiper/react";
+import {Mousewheel} from "swiper/modules";
+import {SwiperNavigation} from "../../utils/SwiperNavigation";
+import {useMediaQuery} from "react-responsive";
+
+interface IProps extends IWithChildren, IWithClass{
+
+}
+
+const BigSlider = (props: IProps) => {
+  const mobileScreen = useMediaQuery({maxWidth: 660});
+  const swiperRef=useRef<SwiperRef | null>(null);
+  const swiperNav= new SwiperNavigation(swiperRef)
+  const [swiperIsStart, setSwiperIsStart] = useState(true)
+  const [swiperIsEnd, setSwiperIsEnd] = useState(false)
+
+  function togleSwiper  (){
+    setSwiperIsStart(swiperNav.isStart())
+    setSwiperIsEnd(swiperNav.isEnd(mobileScreen? 1:2))
+  }
+
+  return (
+      <div className="big-slider">
+        <button
+            className={classNames(
+                "big-slider__btn",
+                "big-slider__btn--prev",
+                swiperIsStart && "btn--disable"
+            )}
+            onClick={() => swiperNav.goToPrev()}
+        >
+          <ReactSVG src="/Assets/Icons/arrow.svg"/>
+        </button>
+        <Swiper
+            slidesPerView="auto"
+            mousewheel={{sensitivity: 5000}}
+            modules={[Mousewheel]}
+            spaceBetween={"10rem"}
+            ref={swiperRef}
+            onActiveIndexChange={togleSwiper}
+        >
+          {props.children}
+        </Swiper>
+        <button
+            className={classNames(
+                "big-slider__btn",
+                "big-slider__btn--next",
+                swiperIsEnd && "btn--disable"
+            )}
+            onClick={() => swiperNav.goToNext()}
+        >
+          <ReactSVG src="/Assets/Icons/arrow.svg"/>
+        </button>
+      </div>
+
+  );
+};
+
+export default BigSlider;
