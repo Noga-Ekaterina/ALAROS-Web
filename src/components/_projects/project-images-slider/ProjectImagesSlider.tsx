cc191fe3bdb3db27230286ff1aef9fb6c24d2f4e
Noga-Ekaterina <katrin210607@gmail.com>
2025-02-15 01:53:06 +0300

    добавила функцию масштабирования в модалку проекта

--- src/components/_projects/project-images-slider/ProjectImagesSlider.tsx
+++ src/components/_projects/project-images-slider/ProjectImagesSlider.tsx
@@ -8,10 +8,33 @@ import {ReactSVG} from "react-svg";
 import {useRouter} from "next/navigation";
 import {useMediaQuery} from "react-responsive";
 import classNames from "classnames";
+import {TransformComponent, TransformWrapper} from "react-zoom-pan-pinch";
+import cn from "classnames";
 
 interface Props{
   project: IProject
 }
+
+// Замените объявление типа в компоненте:
+type zoomFunc = (
+    step?: number,
+    animationTime?: number,
+    animationType?:
+        | 'easeOut'
+        | 'linear'
+        | 'easeInQuad'
+        | 'easeOutQuad'
+        | 'easeInOutQuad'
+        | 'easeInCubic'
+        | 'easeOutCubic'
+        | 'easeInOutCubic'
+        | 'easeInQuart'
+        | 'easeOutQuart'
+        | 'easeInOutQuart'
+        | 'easeInQuint'
+        | 'easeOutQuint'
+        | 'easeInOutQuint'
+) => void;
 
 const ProjectImagesSlider = ({project}:Props) => {
   const router= useRouter()
@@ -21,6 +44,7 @@ const ProjectImagesSlider = ({project}:Props) => {
   const [swiperIsEnd, setSwiperIsEnd] = useState(false)
   const [activeSlide, setActiveSlide] = useState(1)
   const mobileScreen = useMediaQuery({maxWidth: 660});
+  const [scale, setScale] =useState(1)
 
   function togleSwiper  (){
     setSwiperIsStart(swiperNav.isStart())
@@ -35,11 +59,68 @@ const ProjectImagesSlider = ({project}:Props) => {
       document.documentElement.style.overflow=""
     }
   }, []);
+
+  const scaleIn=(zoomIn: zoomFunc)=>{
+    setScale(prevState => prevState + 0.5)
+
+    if ((scale>1.5 || mobileScreen) && scale<3) {
+      zoomIn(0.5)
+    }
+  }
+
+  const scaleOut=(zoomOut: zoomFunc)=>{
+    if (scale>1) {
+      setScale(prevState => prevState - 0.5)
+      zoomOut(0.5)
+    }
+  }
+
   return (
       <div className="project-images-slider">
+        <TransformWrapper >
+          {({ zoomIn, zoomOut, resetTransform }) => (
+              <>
+                <div className="project-images-slider__scale">
+                  <button
+                      className="project-images-slider__btn project-images-slider__btn--increase"
+                      onClick={()=> scaleIn(zoomIn)}
+                  >
+                    <ReactSVG src="/Assets/Icons/close.svg"/>
+                  </button>
+                  <button
+                      className="project-images-slider__btn project-images-slider__btn--decrease"
+                      onClick={()=>scaleOut(zoomOut)}
+                  />
+                </div>
+
+                <div
+                    className={cn(
+                        "project-images-slider__scale-img-wrap",
+                        scale>1 &&"project-images-slider__scale-img-wrap--opened"
+                    )}
+                >
+                  <TransformComponent
+                      wrapperStyle={{maxHeight: "100vh"}}
+                    contentStyle={{cursor: "move"}}
+                  >
+                    {
+                      scale > 1 ?
+                          <div className="">
+                            <img
+                                src={`/Assets/Projects/${project.year}/Project_${project.number}/${project.images[activeSlide - 1]}`}
+                                alt=""/>
+                          </div>
+                          :<div/>
+                    }
+                  </TransformComponent>
+
+                </div>
+              </>
+          )}
+        </TransformWrapper>
         <button
-            className="project-images-slider__back"
-            onClick={()=> router.back()}
+            className="project-images-slider__btn project-images-slider__btn--back"
+            onClick={() => scale>1? setScale(1):router.back()}
         >
           <ReactSVG src="/Assets/Icons/close.svg"/>
         </button>
@@ -75,7 +156,7 @@ const ProjectImagesSlider = ({project}:Props) => {
         >
           {
             project.images.map((image, index) => (
-                <SwiperSlide key={index}>
+                <SwiperSlide key={index} onClick={()=> index===activeSlide-1 && setScale(1.5)}>
                   <img src={`/Assets/Projects/${project.year}/Project_${project.number}/${image}`} alt=""/>
                 </SwiperSlide>
             ))
