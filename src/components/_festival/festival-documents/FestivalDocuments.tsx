3ed24e849624f542e1ef3d99385995c208daa929
Noga-Ekaterina <katrin210607@gmail.com>
2025-02-22 01:42:22 +0300

    вынесла блок со ссылкой в отдельный компонент

--- src/components/_festival/festival-documents/FestivalDocuments.tsx
+++ src/components/_festival/festival-documents/FestivalDocuments.tsx
@@ -1,13 +1,10 @@
 'use client'
-import React, {Fragment, useEffect, useState} from 'react';
+import React from 'react';
 import "./festival-documents.scss"
-import HtmlProcessing from "../../HtmlProcessing";
-import {IFestival, IHtmlString, INomination} from "@/types/data";
+import {IFestival, INomination} from "@/types/data";
 import {getBtns} from "@/utils/getBtns";
 import FestivalTemplates from "@/components/_festival/festival-templates/FestivalTemplates";
-import {useMediaQuery} from "react-responsive";
-import {ReactSVG} from "react-svg";
-import parse from "html-react-parser";
+import LinkBlock from "@/components/link-block/LinkBlock";
 
 interface Props{
   festivalText: IFestival
@@ -17,30 +14,6 @@ interface Props{
 const FestivalDocuments = ({festivalText, nominations}: Props) => {
   const btns= getBtns(festivalText.documentsLinks)
 
-  const getLinks=(mobile: boolean)=>(
-      btns.map(btn=>{
-        const jsx= parse(btn.link)
-        const link=<HtmlProcessing html={btn.link}/>
-        const icon= <span className="festival-documents__icon">[<ReactSVG src='/Assets/Icons/arrow.svg' className="festival-documents__arrow"/>]</span>
-        console.log(link)
-        return (
-            <Fragment key={btn.title}>
-              {
-                (!mobile|| typeof jsx==='string' ||Array.isArray(jsx))? <>{link}</>:
-                    React.createElement(jsx.type, jsx.props, icon
-                    )
-              }
-            </Fragment>
-        )
-      })
-  )
-
-  const mobileScreen = useMediaQuery({maxWidth: 660});
-  const [links, setLinks] = useState(getLinks(false))
-
-  useEffect(() => {
-    setLinks(getLinks(mobileScreen))
-  }, [mobileScreen]);
 
   return (
       <div className="festival-documents" id="documents">
@@ -53,10 +26,7 @@ const FestivalDocuments = ({festivalText, nominations}: Props) => {
             {
               btns.map((btn, index)=>{
                 return (
-                    <div className="festival-documents__btn" key={btn.title}>
-                      <HtmlProcessing html={btn.title}/>
-                      {links[index]}
-                    </div>
+                    <LinkBlock title={btn.title} link={btn.link} className="festival-documents__btn" key={index}/>
                 )
               })
             }
---
+++ src/components/link-block/LinkBlock.tsx
@@ -0,0 +1,57 @@
+'use client'
+import React, {Fragment, JSX, useEffect, useState} from 'react';
+import "./link-block.scss"
+import HtmlProcessing from "@/components/HtmlProcessing";
+import parse from "html-react-parser";
+import {ReactSVG} from "react-svg";
+import {useMediaQuery} from "react-responsive";
+import {IWithClass} from "@/types/tehnic";
+import cn from "classnames";
+
+interface Props extends IWithClass{
+  title: string
+  link: string
+  mobileIcon?: string|JSX.Element
+}
+
+const LinkBlock = ({title, link, mobileIcon, className}:Props) => {
+  const getLinkElement=(mobile: boolean)=>{
+    const jsx= parse(link)
+    const icon= (
+        <span className="link-block__icon">
+          [
+          {
+            mobileIcon || <ReactSVG src='/Assets/Icons/arrow.svg' className="link-block__arrow"/>
+          }
+          ]
+        </span>
+    )
+    console.log(link)
+    return (
+        <>
+          {
+            (!mobile|| typeof jsx==='string' ||Array.isArray(jsx))? <HtmlProcessing html={link}/>:
+                React.createElement(jsx.type, jsx.props, icon
+                )
+          }
+        </>
+    )
+  }
+
+  const mobileScreen = useMediaQuery({maxWidth: 660});
+  const [linkElement, setLinkElement] = useState(getLinkElement(false))
+
+  useEffect(() => {
+    setLinkElement(getLinkElement(mobileScreen))
+  }, [mobileScreen]);
+
+
+  return (
+      <div className={cn("link-block", className)}>
+        <HtmlProcessing html={title}/>
+        {linkElement}
+      </div>
+  );
+};
+
+export default LinkBlock;
