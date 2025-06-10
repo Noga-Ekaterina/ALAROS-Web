081225b0c2bc4bf345e131847af2338812fa1251
Noga-Ekaterina <katrin210607@gmail.com>
2025-06-10 16:51:49 +0300

    смасштабировала шапку и изменила размер загов

--- src/hoocs/useGetHashPosition.ts
+++ src/hoocs/useGetHashPosition.ts
@@ -1,8 +1,10 @@
 import {useGetRem} from "./useGetRem";
 import {useCallback} from "react";
+import {useMediaQuery} from "react-responsive";
 
 export const useGetHashPosition=()=>{
   const rem= useGetRem()
+  const mobileScreen =useMediaQuery({maxWidth: 1024})
 
   return useCallback((hash: string)=>{
     const el= document.getElementById(hash.slice(1))
@@ -10,7 +12,7 @@ export const useGetHashPosition=()=>{
 
     const {top} = el.getBoundingClientRect()
 
-    return top - 48 * rem
+    return top - 48 * rem * (mobileScreen? 1.3:1)
   }, [rem])
 }
 
