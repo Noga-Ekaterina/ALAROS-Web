afa1182a44332edd9aa43cca781046068dc3e804
Noga-Ekaterina <katrin210607@gmail.com>
2025-08-27 20:11:07 +0300

    поставил видео через фрейм

--- src/components/_about/about-history/AdditionItem.tsx
+++ src/components/_about/about-history/AdditionItem.tsx
@@ -7,16 +7,11 @@ interface Props{
 
 const AdditionItem = ({data}: Props) => {
   const html = data.replace(
-      /<h1>(IMG|VIDEO)<\/h1>\s*<p>(.*?)<\/p>/gi,
+      /<h1>(IMG)<\/h1>\s*<p>(.*?)<\/p>/gi,
       (match, type, path) => {
-        if (type.toUpperCase() === 'IMG') {
-          return `<img src="/Assets/Pages/About/History/Additions/${path}">`;
-        } else if (type.toUpperCase() === 'VIDEO') {
-          return `<div class="about-history__video"><video src="/Assets/Pages/About/History/Additions/${path}" controls></video></div>`;
-        }
-        return match; // Возвращаем оригинал, если тип не распознан
+        return `<img src="/Assets/Pages/About/History/Additions/${path}">`;
       }
-  );
+      );
 
   return (
       <div className="about-history__addition">
