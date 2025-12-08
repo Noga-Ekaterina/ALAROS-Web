174bb16d344e9294c9bc719ba278a31298a6c9a7
Noga-Ekaterina <katrin210607@gmail.com>
2025-12-08 20:19:54 +0300

    прокрутка годов в результатах

--- src/components/_competition-results/results/Results.tsx
+++ src/components/_competition-results/results/Results.tsx
@@ -1,9 +1,11 @@
-'use client'
+  'use client'
 import React, {useMemo, useState} from 'react';
 import "./results.scss"
 import {ICompetitionResults} from "@/types/data";
 import {nonBreakingSpaces} from "@/utils/nonBreakingSpaces";
 import {getRowsInTable} from "@/utils/getRowsInTable";
+  import {Swiper, SwiperSlide} from "swiper/react";
+  import {useGetRem} from "@/hoocs/useGetRem";
 
 interface Props{
   pageData: ICompetitionResults
@@ -22,20 +24,24 @@ const getResultsData=(table: string)=>{
 const Results = ({pageData}:Props) => {
   const results= useMemo(() => getResultsData(pageData.results.html), [])
   const [activeYear, setActiveYear] = useState(results[results.length-1].year)
+  const rem=useGetRem()
+
   return (
       <div className="results">
         <div className="container">
           <div className="titles-block">
             <h2 className="titles-block__title titles-block__title--small results__title">{nonBreakingSpaces(pageData.resultsTitle)}</h2>
           </div>
-
-         <div className="results__years">
-           {
-             results.map(({year})=>(
-                 <button key={year} className="results__btn link-underline" disabled={year===activeYear} onClick={()=> setActiveYear(year)}>{year}</button>
-             ))
-           }
-         </div>
+          <Swiper slidesPerView="auto" spaceBetween={35*rem} className="results__years" initialSlide={results.length-1}>
+             {
+               results.map(({year})=>(
+                   <SwiperSlide key={year} style={{width: "fit-content"}}>
+                     <button className="results__btn link-underline" disabled={year === activeYear}
+                             onClick={() => setActiveYear(year)}>{year}</button>?
+                   </SwiperSlide>
+               ))
+             }
+          </Swiper>
 
           <div data-lenis-prevent="">
             <iframe src={results.find(({year}) => year === activeYear)?.link} className="results__iframe"></iframe>
