da029febd3246b13729837c7083d98b6336f6db0
Noga-Ekaterina <katrin210607@gmail.com>
2026-03-01 00:35:17 +0300

    бегушая строка

--- src/components/documents-details/DocumentsDetails .tsx
+++ src/components/documents-details/DocumentsDetails .tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useCallback, useRef, useState} from 'react';
 import "./documents-details.scss"
 import {IButtonBlock, IHtml} from "@/types/data";
 import {getBtns} from "@/utils/getBtns";
 import HtmlProcessing from "@/components/HtmlProcessing";
 import Detalis from "@/components/detalis/Detalis";
+import Marquee from "react-fast-marquee";
 
 interface Props{
   button: IButtonBlock;
@@ -12,11 +13,36 @@ interface Props{
 }
 
 const DocumentsDetails = ({button, links, isDisabled}: Props) => {
+  const titleRef=useRef<null|HTMLDivElement>(null)
+  const titleTextRef=useRef<null|HTMLParagraphElement>(null)
+  const [isPlay, setIsPlay] = useState(false)
+  const setTitleTextRef = useCallback((node: HTMLParagraphElement | null) => {
+    titleTextRef.current = node;
+    if (node && titleRef.current) {
+
+      const checkTruncation=()=>{
+        if (!titleRef.current) return
+        console.log(titleRef.current)
+        setIsPlay(node.clientWidth>titleRef.current?.clientWidth)
+      }
+      checkTruncation();
+      const observer = new ResizeObserver(checkTruncation);
+      observer.observe(node);
+      // cleanup при размонтировании или замене узла
+      return () => observer.disconnect();
+    }
+  }, [titleRef]); // зависимость от titleRef, если он тоже может меняться
 
   return (
       <div className="documents-details">
         <Detalis
-            title={<p>{button.left}</p>}
+            title={
+              <div ref={titleRef}>
+                <Marquee direction='left' play={isPlay} speed={25} className="documents-details__running-line">
+                  <p ref={setTitleTextRef}>{button.left}</p>
+                </Marquee>
+              </div>
+            }
             rightElement={button.right}
             isBtnBg
             disabled={isDisabled}
--- src/components/link-block/LinkBlock.tsx
+++ src/components/link-block/LinkBlock.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, {Fragment, JSX, useEffect, useState} from 'react';
+import React, {Fragment, JSX, useCallback, useEffect, useRef, useState} from 'react';
 import "./link-block.scss"
 import HtmlProcessing from "@/components/HtmlProcessing";
 import parse from "html-react-parser";
@@ -7,6 +7,8 @@ import {ReactSVG} from "react-svg";
 import {useMediaQuery} from "react-responsive";
 import {IWithClass} from "@/types/tehnic";
 import cn from "classnames";
+import Marquee from "react-fast-marquee";
+import {useBreakpoints} from "@/hoocs/useBreakpoints";
 
 interface Props extends IWithClass{
   title: string
@@ -39,15 +41,39 @@ const LinkBlock = ({title, link, mobileIcon, className}:Props) => {
 
   const mobileScreen = useMediaQuery({maxWidth: 660});
   const [linkElement, setLinkElement] = useState(getLinkElement(false))
+  const titleRef=useRef<null|HTMLDivElement>(null)
+  const titleTextRef=useRef<null|HTMLParagraphElement>(null)
+  const [isPlay, setIsPlay] = useState(false)
+  const setTitleTextRef = useCallback((node: HTMLParagraphElement | null) => {
+    titleTextRef.current = node;
+    if (node && titleRef.current) {
+
+      const checkTruncation=()=>{
+        if (!titleRef.current) return
+        console.log(titleRef.current)
+        setIsPlay(node.clientWidth>titleRef.current?.clientWidth)
+      }
+      checkTruncation();
+      const observer = new ResizeObserver(checkTruncation);
+      observer.observe(node);
+      // cleanup при размонтировании или замене узла
+      return () => observer.disconnect();
+    }
+  }, [titleRef]); // зависимость от titleRef, если он тоже может меняться
 
   useEffect(() => {
     setLinkElement(getLinkElement(mobileScreen))
   }, [mobileScreen]);
 
-
   return (
       <div className={cn("link-block", className)}>
-        <div className="link-block__title">{title}</div>
+        <div className="link-block__title" ref={titleRef}>
+          <Marquee direction='left' play={isPlay} speed={25} className="link-block__running-line">
+            <p ref={setTitleTextRef}>
+              {title}
+            </p>
+          </Marquee>
+        </div>
         <div className="link-block__link">{linkElement}</div>
       </div>
   );
