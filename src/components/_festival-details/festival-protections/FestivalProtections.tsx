c042cab7613333c3c8b2dd0ffbfa0746445a5c7e
Noga-Ekaterina <katrin210607@gmail.com>
2026-03-31 23:17:20 +0300

    сделала чтобы заги таблицы оставались вверху

--- src/components/_festival-details/festival-protections/FestivalProtections.tsx
+++ src/components/_festival-details/festival-protections/FestivalProtections.tsx
@@ -4,7 +4,7 @@ import "./festival-protections.scss"
 import Detalis from "../../detalis/Detalis";
 import HtmlProcessing from "../../HtmlProcessing";
 import {createDate, formaterDate} from "../../../utils/date";
-import {IHtmlString, IProtectionsDay} from "@/types/data";
+import {IProtectionsDay} from "@/types/data";
 import {nonBreakingSpaces} from "@/utils/nonBreakingSpaces";
 import SmoothScrolling from "@/app/SmoothScrolling";
 import HorizontalScrollSection from "@/app/HorizontalScrollSection";
@@ -18,7 +18,7 @@ interface Props {
 }
 
 const FestivalProtections = ({title, protectionsDays, protectionsRightSignature, protectionsColumns}:Props) => {
-  const titles=useMemo(()=> [...protectionsColumns].slice(0, 6), [])
+  const titles=useMemo(()=> [...protectionsColumns].slice(0, 6), [protectionsColumns])
   return (
       <div className="festival-protections" id="protections">
         <div className="container">
@@ -37,44 +37,50 @@ const FestivalProtections = ({title, protectionsDays, protectionsRightSignature,
               return(
                   <Detalis  key={`${item.date}-${item.place}`} title={<p className="festival-protections__title-item">
                     <span>{formaterDate(item.date)} | {day}</span> <span className="festival-protections__place">{item.place}</span></p>} startIsOpen={index === 0}>
-                    <SmoothScrolling enableScrollTransfer={true}>
-                      <HorizontalScrollSection>
-                        <table>
-                          <thead>
-                            <tr>
-                              {
-                                titles.map((str, titleIndex)=>(
-                                    <th key={titleIndex}>{nonBreakingSpaces(str)}</th>
-                                ))
-                              }
-                            </tr>
-                          </thead>
-                          <tbody>
+                    <HorizontalScrollSection>
+                      <div className="festival-protections__table">
+                        <div className="festival-protections__head festival-protections__row">
                           {
-                            item.protections.map((protection, protectionIndex)=>(
-                                <tr key={protectionIndex} className={cn({"grey": protection.isBreak})}>
-                                  <td>{protection.time}</td>
-                                  {
-                                    protection.isBreak?
-                                        <td>{protection.breakTitle}</td>
-                                    :
-                                        <>
-                                          <td className="red">
-                                            {protection.isOnline &&"online"}
-                                          </td>
-                                          <td>{protection.number}</td>
-                                          <td>{protection.nomination}</td>
-                                          <td>{protection.winner}</td>
-                                          <td>{protection.name}</td>
-                                        </>
-                                  }
-                                </tr>
+                            titles.map((str, titleIndex)=>(
+                                <div key={titleIndex} className="festival-protections__cell festival-protections__cell--head">
+                                  {nonBreakingSpaces(str)}
+                                </div>
                             ))
                           }
-                          </tbody>
-                        </table>
-                      </HorizontalScrollSection>
-                    </SmoothScrolling>
+                        </div>
+
+                        <SmoothScrolling enableScrollTransfer={true}>
+                          <div className="festival-protections__body">
+                            {
+                              item.protections.map((protection, protectionIndex)=>(
+                                  <div
+                                      key={protectionIndex}
+                                      className={cn("festival-protections__row", {
+                                        "festival-protections__row--break": protection.isBreak
+                                      })}
+                                  >
+                                    <div className="festival-protections__cell">{protection.time}</div>
+                                    {
+                                      protection.isBreak?
+                                          <div className="festival-protections__cell">{protection.breakTitle}</div>
+                                      :
+                                          <>
+                                            <div className="festival-protections__cell festival-protections__cell--red">
+                                              {protection.isOnline &&"online"}
+                                            </div>
+                                            <div className="festival-protections__cell">{protection.number}</div>
+                                            <div className="festival-protections__cell">{protection.nomination}</div>
+                                            <div className="festival-protections__cell">{protection.winner}</div>
+                                            <div className="festival-protections__cell">{protection.name}</div>
+                                          </>
+                                    }
+                                  </div>
+                              ))
+                            }
+                          </div>
+                        </SmoothScrolling>
+                      </div>
+                    </HorizontalScrollSection>
                   </Detalis>
                     )
                   })
